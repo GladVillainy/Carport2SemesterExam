715f3373fe815747274c2c2a8527e0abfc472cf2
GladVillainy <lucasbuades00@gmail.com>
2026-05-13 14:45:19 +0200

    Updated validator and edited name.
    methods removed: checkAdmin and checkSales. Added "hasRole" which does the same, but more abstract, beause it takes a string, checks if user has that rule then returns with true if user has it, or false if it dosnt have the rule.

--- src/main/java/validators/RoleValidator.java
+++ src/main/java/validators/RoleValidator.java
@@ -2,7 +2,7 @@ package validators;
 import io.javalin.http.Context;
 
 public class RoleValidator {
-    public static boolean checkAdmin(Context ctx) {
+    public static boolean hasRole(Context ctx, String role) {
         //gets customer from session
         entities.Customer customer = ctx.sessionAttribute("currentUser");
 
@@ -10,25 +10,8 @@ public class RoleValidator {
         if(customer == null){
             return false;
         }
-        //Check if customer is an admin
-        if (customer.getRole().equalsIgnoreCase("admin")) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    public static boolean checkSalesman(Context ctx) {
-        //gets customer from session
-        entities.Customer customer = ctx.sessionAttribute("currentUser");
-
-        //check if customer is a guest customer
-        if(customer == null){
-            return false;
-        }
-
-        //Check if customer is a sales
-        if (customer.getRole().equalsIgnoreCase("sales")) {
+        //Check if a user has the given role
+        if (customer.getRole().equalsIgnoreCase(role)) {
             return true;
         } else {
             return false;
