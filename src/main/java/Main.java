1d3e5a2df5c202db738b2e4ed7cc045140b96747
GladVillainy <lucasbuades00@gmail.com>
2026-05-13 14:15:20 +0200

    Introduced main

---
+++ src/main/java/Main.java
@@ -0,0 +1,27 @@
+
+import config.ThymeleafConfig;
+import persistence.ConnectionPool;
+import io.javalin.Javalin;
+import io.javalin.rendering.template.JavalinThymeleaf;
+
+public class Main {
+
+    private static final String USER = "postgres";
+    private static final String PASSWORD = "postgres";
+    private static final String URL = "jdbc:postgresql://localhost:5432/%s?currentSchema=public";
+    private static final String DB = "cupcake";
+
+    private static final ConnectionPool connectionPool = ConnectionPool.getInstance(USER, PASSWORD, URL, DB);
+
+    public static void main(String[] args) {
+        // Initializing Javalin and Jetty webserver
+        Javalin javApp = Javalin.create(config -> {
+            config.staticFiles.add("/public");
+            config.fileRenderer(new JavalinThymeleaf(ThymeleafConfig.templateEngine()));
+            config.staticFiles.add("/templates");
+        }).start(7070);
+
+        // Routing
+
+    }
+}
