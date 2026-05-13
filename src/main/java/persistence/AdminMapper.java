45ba2be84549906ce339416bd1b74d71a08cb2ac
GladVillainy <lucasbuades00@gmail.com>
2026-05-13 15:55:21 +0200

    Introduced new class "AdminMapper"
    first step for crud ability for the admin. These methods are only crud towards users atm.

---
+++ src/main/java/persistence/AdminMapper.java
@@ -0,0 +1,81 @@
+package persistence;
+
+import entities.User;
+import exceptions.DatabaseException;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class AdminMapper {
+    public static void createUser(String email, String password, String address,
+                                  int phoneNumber, String name, String role, ConnectionPool connectionPool) {
+
+        String sql = "INSERT INTO public.users (email, address, phone, password, role) VALUES (?, ?, ?, ?, ?)";
+
+        try (Connection connection = connectionPool.getConnection();
+             PreparedStatement ps = connection.prepareStatement(sql)) {
+
+            ps.setString(1, email);
+            ps.setString(2, address);
+            ps.setInt(3, phoneNumber);
+            ps.setString(4, password);
+            ps.setString(5, role);
+
+            int rowsAffected = ps.executeUpdate();
+            if (rowsAffected != 1) {
+                throw new DatabaseException("Fejl ved oprettelse af ny bruger");
+            }
+        } catch (SQLException e) {
+            String msg = "Der er sket en fejl. Prøv igen";
+            if (e.getMessage().startsWith("ERROR: duplicate key value ")) {
+                msg = "E-mail findes allerede. Vælg en anden";
+            }
+            throw new DatabaseException("DB fejl: " + e.getMessage() + " (" + msg + ")");
+        }
+    }
+
+    public static void editUserByID(int id, String email, String password, String address,
+                                    int phoneNumber, String name, String role, ConnectionPool connectionPool) {
+
+        String sql = "UPDATE public.users SET email = ?, address = ?, phone = ?, password = ?, role = ? WHERE user_id = ?";
+
+        try (Connection connection = connectionPool.getConnection();
+             PreparedStatement ps = connection.prepareStatement(sql)) {
+
+            ps.setString(1, email);
+            ps.setString(2, address);
+            ps.setInt(3, phoneNumber);
+            ps.setString(4, password);
+            ps.setString(5, role);
+            ps.setInt(6, id);
+
+            int rowsAffected = ps.executeUpdate();
+            if (rowsAffected != 1) {
+                throw new DatabaseException("Fejl opstået ved ændring af brugeren");
+            }
+        } catch (SQLException e) {
+            String msg = "Der er sket en fejl. Prøv igen";
+            throw new DatabaseException("DB fejl: " + e.getMessage() + " (" + msg + ")");
+        }
+    }
+
+    public static void deleteUserByID(int id, ConnectionPool connectionPool) {
+        String sql = "DELETE FROM public.users WHERE user_id = ?";
+
+        try (Connection connection = connectionPool.getConnection();
+             PreparedStatement ps = connection.prepareStatement(sql)) {
+
+            ps.setInt(1, id);
+
+            int rowsAffected = ps.executeUpdate();
+            if (rowsAffected != 1) {
+                throw new DatabaseException("Fejl opstået ved sletningen af brugeren");
+            }
+        } catch (SQLException e) {
+            String msg = "Der er sket en fejl. Prøv igen";
+            throw new DatabaseException("DB fejl: " + e.getMessage() + " (" + msg + ")");
+        }
+    }
+}
