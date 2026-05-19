71f7e78c3d0d58d68a2fbea90de2914aa75021e5
GladVillainy <lucasbuades00@gmail.com>
2026-05-19 17:41:43 +0200

    Updated order to be able to hold users

--- src/main/java/entities/Order.java
+++ src/main/java/entities/Order.java
@@ -1,11 +1,13 @@
 package entities;
 
+
+
 import java.util.ArrayList;
-import java.util.List;
 
 public class Order {
     private ArrayList<Carport> carports = new ArrayList<>();
     private ArrayList<TotalOrderLines> orderLines = new ArrayList<>();
+    private User customer;
 
     private int id;
     private double totalPrice;
@@ -13,48 +15,31 @@ public class Order {
 
     //To create
     public Order(ArrayList<Carport> carports, ArrayList<TotalOrderLines> orderLines,
-                 double totalPrice, String status) {
+                 User customer, double totalPrice, String status) {
         this.carports = carports;
         this.orderLines = orderLines;
+        this.customer = customer;
         this.totalPrice = totalPrice;
         this.status = status;
     }
 
     //To read
     public Order(ArrayList<Carport> carports, ArrayList<TotalOrderLines> orderLines,
-                 int id, double totalPrice, String status) {
+                 User customer, int id, double totalPrice, String status) {
         this.carports = carports;
         this.orderLines = orderLines;
+        this.customer = customer;
         this.id = id;
         this.totalPrice = totalPrice;
         this.status = status;
     }
 
-    public ArrayList<Carport> getCarports() {
-        return carports;
-    }
-
-    public ArrayList<TotalOrderLines> getOrderLines() {
-        return orderLines;
-    }
-
-    public double getTotalPrice() {
-        return totalPrice;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setTotalPrice(double totalPrice) {
-        this.totalPrice = totalPrice;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
-    public int getId() {
-        return id;
-    }
+    public ArrayList<Carport> getCarports() { return carports; }
+    public ArrayList<TotalOrderLines> getOrderLines() { return orderLines; }
+    public User getCustomer() { return customer; }
+    public int getId() { return id; }
+    public double getTotalPrice() { return totalPrice; }
+    public String getStatus() { return status; }
+    public void setTotalPrice(double totalPrice) { this.totalPrice = totalPrice; }
+    public void setStatus(String status) { this.status = status; }
 }
