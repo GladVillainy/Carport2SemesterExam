6284841505f03afcc9cd0fad5c1d4d065d1d6f27
GladVillainy <lucasbuades00@gmail.com>
2026-05-21 15:40:32 +0200

    Removed admin in methods and fixed typos
    I removed admin check in methods due to being unnecessary if we put the check in routing instead.
    
    Also fixed typos in comments
    
    I removed the "todo" in createUser since i made it when doing the login/create user taks

--- src/main/java/controller/AdminController.java
+++ src/main/java/controller/AdminController.java
@@ -36,211 +36,167 @@ public class AdminController {
         app.post("/seeAllMaterial", ctx -> seeAllMaterial(ctx, connectionPool));
 
     }
-
-    //TODO check if mikkel already made this
+    
     public static void createUser(Context ctx, ConnectionPool connectionPool) {
 
     }
 
     public static void deleteUserByID(Context ctx, ConnectionPool connectionPool) {
-        //Checks if user is admin
-        if (RoleValidator.hasRole(ctx, "admin")) {
-
-            //Verifyes if ID is empty or not a number, to prevent NumberFormatException
-            String idInput = ctx.formParam("user_id");
-            if (InputValidator.isItEmpty(idInput) || !InputValidator.isNumeric(idInput)) {
-                ctx.attribute("errorMessage", "Ugyldigt bruger ID");
-                ctx.redirect("/adminUserCRUD");
-                return;
-            }
-
-            //Parses ID and sends to mapper for delettion
-            int id = Integer.parseInt(idInput);
-            AdminMapper.deleteCustomerByID(id, connectionPool);
+        //Verifyes if ID is empty or not a number, to prevent NumberFormatException
+        String idInput = ctx.formParam("user_id");
+        if (InputValidator.isItEmpty(idInput) || !InputValidator.isNumeric(idInput)) {
+            ctx.attribute("errorMessage", "Ugyldigt bruger ID");
             ctx.redirect("/adminUserCRUD");
-        } else {
-            ctx.redirect("/index");
+            return;
         }
+        //Parses ID and sends to mapper for delettion
+        int id = Integer.parseInt(idInput);
+        AdminMapper.deleteCustomerByID(id, connectionPool);
+        ctx.redirect("/adminUserCRUD");
     }
 
     public static void editUserByID(Context ctx, ConnectionPool connectionPool) {
-        //Checks if user is admin
-        if (RoleValidator.hasRole(ctx, "admin")) {
-
-            //Verifyes if ID is empty or not a number, to prevent NumberFormatException
-            String idInput = ctx.formParam("user_id");
-            if (InputValidator.isItEmpty(idInput) || !InputValidator.isNumeric(idInput)) {
-                ctx.attribute("errorMessage", "Ugyldigt bruger ID");
-                ctx.redirect("/adminUserCRUD");
-                return;
-            }
-
-            //Verifyes if phone isnt empty and isnt a number, to prevent NumberFormatException
-            String phoneNumber = ctx.formParam("phoneNumber");
-            if (!InputValidator.isItEmpty(phoneNumber) && !InputValidator.isNumeric(phoneNumber)) {
-                ctx.attribute("errorMessage", "Telefonnummer skal være et tal");
-                ctx.redirect("/adminUserCRUD");
-                return;
-            }
-
-            //Gets remaining data and sends to mapper for editing
-            String email = ctx.formParam("email");
-            String password = ctx.formParam("password");
-            String address = ctx.formParam("address");
-            String role = ctx.formParam("role");
-            int id = Integer.parseInt(idInput);
-
-            AdminMapper.editCustomerByID(id, email, password, address, phoneNumber, role, connectionPool);
+        //Verifyes if ID is empty or not a number, to prevent NumberFormatException
+        String idInput = ctx.formParam("user_id");
+        if (InputValidator.isItEmpty(idInput) || !InputValidator.isNumeric(idInput)) {
+            ctx.attribute("errorMessage", "Ugyldigt bruger ID");
             ctx.redirect("/adminUserCRUD");
-        } else {
-            ctx.redirect("/index");
+            return;
         }
+
+        //Verifyes if phone isnt empty and isnt a number, to prevent NumberFormatException
+        String phoneNumber = ctx.formParam("phoneNumber");
+        if (!InputValidator.isItEmpty(phoneNumber) && !InputValidator.isNumeric(phoneNumber)) {
+            ctx.attribute("errorMessage", "Telefonnummer skal være et tal");
+            ctx.redirect("/adminUserCRUD");
+            return;
+        }
+
+        //Gets remaining data and sends to mapper for editing
+        String email = ctx.formParam("email");
+        String password = ctx.formParam("password");
+        String address = ctx.formParam("address");
+        String role = ctx.formParam("role");
+        int id = Integer.parseInt(idInput);
+
+        AdminMapper.editCustomerByID(id, email, password, address, phoneNumber, role, connectionPool);
+        ctx.redirect("/adminUserCRUD");
     }
 
-    public static void showAllCustomers(Context ctx, ConnectionPool connectionPool) {
-        //Checks if user is admin
-        if (RoleValidator.hasRole(ctx, "admin")) {
 
-            //Gets all users and filters so we only see customers
-            List<Customer> allCustomers = AdminMapper.getAllRegistered(connectionPool);
-            allCustomers = allCustomers.stream()
-                    .filter(u -> u.getRole().equalsIgnoreCase("customer"))
-                    .sorted(Comparator.comparing(Customer::getEmail))
-                    .toList();
-            ctx.sessionAttribute("allCustomer", allCustomers);
-            ctx.redirect("/adminUserCRUD");
-        } else {
-            ctx.redirect("/index");
-        }
+    public static void showAllCustomers(Context ctx, ConnectionPool connectionPool) {
+        //Gets all users and filters so we only see customers
+        List<Customer> allCustomers = AdminMapper.getAllRegistered(connectionPool);
+        allCustomers = allCustomers.stream()
+                .filter(u -> u.getRole().equalsIgnoreCase("customer"))
+                .sorted(Comparator.comparing(Customer::getEmail))
+                .toList();
+        ctx.sessionAttribute("allCustomer", allCustomers);
+        ctx.redirect("/adminUserCRUD");
+
     }
 
     public static void showAllStaff(Context ctx, ConnectionPool connectionPool) {
-        //Checks if user is admin
-        if (RoleValidator.hasRole(ctx, "admin")) {
+        //Gets all users and filters so we only see admins and sales
+        List<Customer> allCustomers = AdminMapper.getAllRegistered(connectionPool);
+        allCustomers = allCustomers.stream()
+                .filter(u -> u.getRole().equalsIgnoreCase("admin")
+                        || u.getRole().equalsIgnoreCase("sales"))
+                .sorted(Comparator.comparing(Customer::getEmail))
+                .toList();
+        ctx.sessionAttribute("allCustomer", allCustomers);
+        ctx.redirect("/adminUserCRUD");
 
-            //Gets all users and filters so we only see admins and sales
-            List<Customer> allCustomers = AdminMapper.getAllRegistered(connectionPool);
-            allCustomers = allCustomers.stream()
-                    .filter(u -> u.getRole().equalsIgnoreCase("admin")
-                            || u.getRole().equalsIgnoreCase("sales"))
-                    .sorted(Comparator.comparing(Customer::getEmail))
-                    .toList();
-            ctx.sessionAttribute("allCustomer", allCustomers);
-            ctx.redirect("/adminUserCRUD");
-        } else {
-            ctx.redirect("/index");
-        }
     }
 
     //Materiale //
 
     public static void createMaterial(Context ctx, ConnectionPool connectionPool) {
-        //Checks if user is admin
-        if (RoleValidator.hasRole(ctx, "admin")) {
+        //Gets data
+        String name = ctx.formParam("name");
+        String description = ctx.formParam("description");
 
-            //Gets data
-            String name = ctx.formParam("name");
-            String description = ctx.formParam("description");
-
-            //Verifyes if price is either empty or isnt a number, to prevent NumberFormatException.
-            String priceInput = ctx.formParam("price");
-            if (InputValidator.isItEmpty(priceInput)) {
-                ctx.attribute("errorMessage", "Pris skal udfyldes, og være et tal");
-                ctx.render("adminMaterialCRUD.html");
-                return;
-            } else if (!InputValidator.isNumeric(priceInput)) {
-                ctx.attribute("errorMessage", "Pris skal være et tal");
-                ctx.render("adminMaterialCRUD.html");
-                return;
-            }
-
-            //Verifyes if length isnt empty and isnt a number, to prevent NumberFormatException.
-            String lengthInput = ctx.formParam("length");
-            if (!InputValidator.isItEmpty(lengthInput) && !InputValidator.isNumeric(lengthInput)) {
-                ctx.attribute("errorMessage", "Længde skal være et tal");
-                ctx.render("adminMaterialCRUD.html");
-                return;
-            }
-
-            //Sends data to mapper for creation
-            AdminMapper.createMaterial(name, priceInput, description, lengthInput, connectionPool);
-            ctx.redirect("/adminMaterialCRUD");
-        } else {
-            ctx.redirect("/index");
+        //Verifies if price is either empty or isnt a number, to prevent NumberFormatException.
+        String priceInput = ctx.formParam("price");
+        if (InputValidator.isItEmpty(priceInput)) {
+            ctx.attribute("errorMessage", "Pris skal udfyldes, og være et tal");
+            ctx.render("adminMaterialCRUD.html");
+            return;
+        } else if (!InputValidator.isNumeric(priceInput)) {
+            ctx.attribute("errorMessage", "Pris skal være et tal");
+            ctx.render("adminMaterialCRUD.html");
+            return;
         }
+
+        //Verifies if length isnt empty and isnt a number, to prevent NumberFormatException.
+        String lengthInput = ctx.formParam("length");
+        if (!InputValidator.isItEmpty(lengthInput) && !InputValidator.isNumeric(lengthInput)) {
+            ctx.attribute("errorMessage", "Længde skal være et tal");
+            ctx.render("adminMaterialCRUD.html");
+            return;
+        }
+
+        //Sends data to mapper for creation
+        AdminMapper.createMaterial(name, priceInput, description, lengthInput, connectionPool);
+        ctx.redirect("/adminMaterialCRUD");
     }
 
     public static void editMaterialByID(Context ctx, ConnectionPool connectionPool) {
-        //Checks if user is admin
-        if (RoleValidator.hasRole(ctx, "admin")) {
-
-            //Verifyes if material ID is empty or not a number, to prevent NumberFormatException
-            String idInput = ctx.formParam("material_id");
-            if (InputValidator.isItEmpty(idInput) || !InputValidator.isNumeric(idInput)) {
-                ctx.attribute("errorMessage", "Ugyldigt materiale ID");
-                ctx.redirect("/adminMaterialCRUD");
-                return;
-            }
-
-            //Verifyes if price isnt empty and isnt a number, to prevent NumberFormatException
-            String priceInput = ctx.formParam("price");
-            if (!InputValidator.isItEmpty(priceInput) && !InputValidator.isNumeric(priceInput)) {
-                ctx.attribute("errorMessage", "Pris skal være et tal");
-                ctx.redirect("/adminMaterialCRUD");
-                return;
-            }
-
-            //Verifyes if length isnt empty and isnt a number, to prevent NumberFormatException
-            String lengthInput = ctx.formParam("length");
-            if (!InputValidator.isItEmpty(lengthInput) && !InputValidator.isNumeric(lengthInput)) {
-                ctx.attribute("errorMessage", "Længde skal være et tal");
-                ctx.redirect("/adminMaterialCRUD");
-                return;
-            }
-
-            //Gets remaining data and sends to mapper for editing
-            String name = ctx.formParam("name");
-            String description = ctx.formParam("description");
-            int id = Integer.parseInt(idInput);
-
-            AdminMapper.editMaterialByID(name, priceInput, description, lengthInput, id, connectionPool);
+        //Verifies if material ID is empty or not a number, to prevent NumberFormatException
+        String idInput = ctx.formParam("material_id");
+        if (InputValidator.isItEmpty(idInput) || !InputValidator.isNumeric(idInput)) {
+            ctx.attribute("errorMessage", "Ugyldigt materiale ID");
             ctx.redirect("/adminMaterialCRUD");
-        } else {
-            ctx.redirect("/index");
+            return;
         }
+
+        //Verifies if price isnt empty and isnt a number, to prevent NumberFormatException
+        String priceInput = ctx.formParam("price");
+        if (!InputValidator.isItEmpty(priceInput) && !InputValidator.isNumeric(priceInput)) {
+            ctx.attribute("errorMessage", "Pris skal være et tal");
+            ctx.redirect("/adminMaterialCRUD");
+            return;
+        }
+
+        //Verifies if length isnt empty and isnt a number, to prevent NumberFormatException
+        String lengthInput = ctx.formParam("length");
+        if (!InputValidator.isItEmpty(lengthInput) && !InputValidator.isNumeric(lengthInput)) {
+            ctx.attribute("errorMessage", "Længde skal være et tal");
+            ctx.redirect("/adminMaterialCRUD");
+            return;
+        }
+
+        //Gets remaining data and sends to mapper for editing
+        String name = ctx.formParam("name");
+        String description = ctx.formParam("description");
+        int id = Integer.parseInt(idInput);
+
+        AdminMapper.editMaterialByID(name, priceInput, description, lengthInput, id, connectionPool);
+        ctx.redirect("/adminMaterialCRUD");
+
     }
 
     public static void deleteMaterialByID(Context ctx, ConnectionPool connectionPool) {
-        //Checks if user is admin
-        if (RoleValidator.hasRole(ctx, "admin")) {
-
-            //Verifyes if material ID is empty or not a number, to prevent NumberFormatException
-            String idInput = ctx.formParam("material_id");
-            if (InputValidator.isItEmpty(idInput) || !InputValidator.isNumeric(idInput)) {
-                ctx.attribute("errorMessage", "Ugyldigt materiale ID");
-                ctx.redirect("/adminMaterialCRUD");
-                return;
-            }
-
-            //Parses ID and sends to mapper for delettion
-            int id = Integer.parseInt(idInput);
-            AdminMapper.deleteMaterialByID(id, connectionPool);
+        //Verifyes if material ID is empty or not a number, to prevent NumberFormatException
+        String idInput = ctx.formParam("material_id");
+        if (InputValidator.isItEmpty(idInput) || !InputValidator.isNumeric(idInput)) {
+            ctx.attribute("errorMessage", "Ugyldigt materiale ID");
             ctx.redirect("/adminMaterialCRUD");
-        } else {
-            ctx.redirect("/index");
+            return;
         }
+
+        //Parses ID and sends to mapper for delettion
+        int id = Integer.parseInt(idInput);
+        AdminMapper.deleteMaterialByID(id, connectionPool);
+        ctx.redirect("/adminMaterialCRUD");
+
     }
 
     public static void seeAllMaterial(Context ctx, ConnectionPool connectionPool) {
-        //Checks if user is admin
-        if (RoleValidator.hasRole(ctx, "admin")) {
+        //Gets all materials and sends them to session
+        List<Material> allMaterial = AdminMapper.seeAllMaterial(connectionPool);
+        ctx.sessionAttribute("allMaterial", allMaterial);
+        ctx.redirect("/adminUserMaterial");
 
-            //Gets all materials and sends them to session
-            List<Material> allMaterial = AdminMapper.seeAllMaterial(connectionPool);
-            ctx.sessionAttribute("allMaterial", allMaterial);
-            ctx.redirect("/adminUserMaterial");
-        } else {
-            ctx.redirect("/index");
-        }
     }
 }
