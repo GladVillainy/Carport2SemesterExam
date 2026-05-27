cbe593865c8de10839745cf5832ae05027629cb7
MikkelNorden <mikkelhoegnorden@gmail.com>
2026-05-27 17:25:02 +0200

    Slight modification to createCarport() so the returned java object has the correct id

--- src/main/java/persistence/CarportMapper.java
+++ src/main/java/persistence/CarportMapper.java
@@ -1,17 +1,16 @@
 package persistence;
 
 import entities.Carport;
+import entities.Order;
 import exceptions.DatabaseException;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class CarportMapper {
 
     public static Carport createCarport(int order_id, int length, int width, int height, String roofType, boolean shed, ConnectionPool connectionPool) {
 
-        String sql = "INSERT INTO public.carport (order_id, length, width, height, roof_type, shed) VALUES (?, ?, ?, ?, ?, ?)";
+        String sql = "INSERT INTO public.carport (order_id, length, width, height, roof_type, shed) VALUES (?, ?, ?, ?, ?, ?) RETURNING carport_id";
 
         try (Connection connection = connectionPool.getConnection();
              PreparedStatement ps = connection.prepareStatement(sql)) {
@@ -23,14 +22,17 @@ public class CarportMapper {
             ps.setString(5, roofType);
             ps.setBoolean(6, shed);
 
-            int rowsAffected = ps.executeUpdate();
-            if (rowsAffected != 1) {
-                throw new DatabaseException("Fejl ved oprettelse af ny carport");
+            ResultSet rs = ps.executeQuery();
+
+            if (rs.next()) {
+                int carportId = rs.getInt("carport_id");
+                return new Carport(carportId, length, width, height, roofType, shed);
             }
+
         } catch (SQLException e) {
             String msg = "Der er sket en fejl. Prøv igen";
             throw new DatabaseException("DB fejl: " + e.getMessage() + " (" + msg + ")");
         }
-        return new Carport(length, width, height, roofType, shed);
+        return null;
     }
 }
